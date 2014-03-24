3cc870b68e96708898f3f0effbaddaadd2eef3b6
Jason Gegere <jason@hgmail.com>
2014-03-24 00:14:04 -0500

    update db table

--- sendgrid_events.sql
+++ sendgrid_events.sql
@@ -6,17 +6,22 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sendgrid_events`;
 CREATE TABLE `sendgrid_events` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `msg_id` int(10) unsigned DEFAULT NULL,
+  `smtp_id` varchar(150) DEFAULT NULL,
   `email` varchar(150) NOT NULL,
-  `attempt` int(11) NOT NULL,
-  `timestamp` timestamp NULL default NULL,
-  `category` varchar(30) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `event` varchar(20) NOT NULL,
-  `response` longtext NOT NULL,
-  `reason` longtext NOT NULL,
-  `url` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+  `attempt` int(11) DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  `category` longtext,
+  `type` varchar(255) DEFAULT NULL,
+  `event` varchar(20) DEFAULT NULL,
+  `response` longtext,
+  `reason` longtext,
+  `url` varchar(255) DEFAULT NULL,
+  `user_agent` varchar(255) DEFAULT NULL,
+  `ip` int(10) DEFAULT NULL,
+  `raw_data` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
